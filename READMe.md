f7eceac896c8a0a0da239a59678571985d943e6a
Thiagolp7 <thiagolp.dev@gmail.com>
2022-05-31 15:40:29 -0300

    Chapter I - Utilizando os métodos HTTP.

--- READMe.md
+++ READMe.md
@@ -13,3 +13,15 @@
 
 - yarn add nodemon -D : para instalar o nodemon como dependência de dev.
 - Adicionamos um script no package.json ""dev": "nodemon src/index.js"", para executarmos o serv com "yarn dev".
+
+### Utilizar os métodos Http
+
+> Criamos algumas rotas para nossa aplicação utilizando os métodos abaixo.
+
+- GET - Buscar uma info dentro do servidor.
+- POST - Inserir uma info no servidor.
+- PUT - Alterar uma informação no servidor.
+- PATCH - Alterar uma info específica.
+- DELETE - Deletar uma info do servidor.
+  > Obs.: Os métodos GET e POST recebem os params de forma opcional de acordo com a rota que queremos acessar.
+  > Os métodos PUT, PATCH e DELETE, precisam receber um param na rota de acordo com o que queremos atualizar ou deletar. Se vamos excluir um user ex.: app.delet("/users/:id", (req, res) => {}).
--- src/index.js
+++ src/index.js
@@ -2,8 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello Wolrd Ignite!' });
+app.get('/courses', (req, res) => {
+  return res.json(['Curso 1', 'Curso 2', 'Curso 3']);
 });
 
-app.listen(3333); // - Define a porta que vamos utilizar: localhost:3333
+app.post('/courses', (req, res) => {
+  return res.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4']);
+});
+
+// Precisamos pegar o id nos params da rota.
+app.put('/courses/:id', (req, res) => {
+  return res.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4']);
+});
+
+app.patch('/courses/:id', (req, res) => {
+  return res.json(['Curso 6', 'Curso 7', 'Curso 3', 'Curso 4']);
+});
+
+app.delete('/courses/:id', (req, res) => {
+  return res.json(['Curso 6', 'Curso 7', 'Curso 4']);
+});
+
+app.listen(3333); // localhost:3333
